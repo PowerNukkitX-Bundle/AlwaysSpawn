5513b5d918981ae73ee1695a5c8b7a8186ef8621
Buddelbubi <44944269+Buddelbubi@users.noreply.github.com>
2026-08-18 22:57:23 +0200

    Implemented AlwaysSpawn

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # AlwaysSpawn
 
-A plugin for [PowerNukkitX](github.com/PowerNukkitX/PowerNukkitX)
+A small [PowerNukkitX](https://github.com/PowerNukkitX/PowerNukkitX) plugin that always sends players to the default world's spawn point when spawning.
+
+## Installation
+
+Place `AlwaysSpawn.jar` in your server's `plugins` directory and restart the server. No configuration is required.
--- src/main/java/org/powernukkitx/alwaysspawn/AlwaysSpawn.java
+++ src/main/java/org/powernukkitx/alwaysspawn/AlwaysSpawn.java
@@ -1,6 +1,14 @@
 package org.powernukkitx.alwaysspawn;
 
+import org.powernukkitx.Server;
+import org.powernukkitx.event.EventHandler;
+import org.powernukkitx.event.EventPriority;
+import org.powernukkitx.event.Listener;
+import org.powernukkitx.event.player.PlayerTeleportEvent;
+import org.powernukkitx.level.Location;
+import org.powernukkitx.level.Position;
 import org.powernukkitx.plugin.PluginBase;
+import org.powernukkitx.plugin.annotation.EventListener;
 import org.powernukkitx.plugin.annotation.PluginMeta;
 
 @PluginMeta(
@@ -14,13 +22,26 @@ import org.powernukkitx.plugin.annotation.PluginMeta;
         },
         website = "https://github.com/PowerNukkitX-Bundle/AlwaysSpawn"
 )
-public class AlwaysSpawn extends PluginBase {
+@EventListener
+public class AlwaysSpawn extends PluginBase implements Listener {
 
     private static AlwaysSpawn INSTANCE;
 
     @Override
     public void onEnable() {
         INSTANCE = this;
+    }
+
+    @EventHandler(priority = EventPriority.LOW)
+    public void onTeleport(PlayerTeleportEvent event) {
+        if (event.getCause() == PlayerTeleportEvent.TeleportCause.PLAYER_SPAWN) {
+            Position spawn = Server.getInstance().getDefaultLevel().getSpawnLocation();
+            Location to = event.getTo();
+            to.setX(spawn.getX());
+            to.setY(spawn.getY());
+            to.setZ(spawn.getZ());
+            to.setLevel(spawn.getLevel());
+        }
     }
 
     public static AlwaysSpawn get() {
